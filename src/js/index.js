ba7b54e393a3cccd259ce4e5ef145bf5d60d4f0c
Gerdeth <gerdethwickham@gmail.com>
2020-07-21 23:19:34 +0000

    initial

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,16 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var num = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  var suite = ["&#9824", "&#9829", "&#9827", "&#9830"];
+  var ret = num[Math.floor(Math.random() * num.length)];
+  var pic = suite[Math.floor(Math.random() * suite.length)];
+
+  if (pic == "&#9829" || pic == "&#9830") {
+    document.querySelector("#card").style.color = "red";
+  }
+
+  document.querySelector(".firImg").innerHTML = pic;
+  document.querySelector(".secImg").innerHTML = pic;
+  document.querySelector(".number").innerHTML = ret;
 };
